0ea273eadf765896459b5fe65690efa69b1a7fcc
nour-alsatari <nouress98@gmail.com>
2022-03-15 15:20:10 +0300

    changed user route to be for admins only and added routes file

--- middlewares/bearer.middleware.js
+++ middlewares/bearer.middleware.js
@@ -3,12 +3,13 @@ const Users = require('../models/users.model');
 
 function bearer(req, res, next) {
 
-    console.log(req.headers.authorization);
+    if(req.headers.authorization);
     const bearerHeaderToken = req.headers.authorization.split(' ')[1]; // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRhbWltIiwiaWF0IjoxNjM2MzY2MDgwfQ.OhHLD4yRWs1LlTloBjIs0j-QYzi8LdoQDXUfPaO0BSg
 
     Users.authenticateBearer(bearerHeaderToken)
         .then(userData => {
             req.user = userData;
+            req.user.token = bearerHeaderToken;
             next();
         }).catch(() => {
             next('Bearer token authentication error');
